469e512e8ffee377316f492873c44f9f03c8e2b3
Shubham Patil <shubhampatilsd@gmail.com>
2024-02-04 13:00:17 -0800

    maps

--- client/components/MainPage.tsx
+++ client/components/MainPage.tsx
@@ -1,18 +1,55 @@
 import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Pressable, Text, View, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 export const MainPage = () => {
-  return (
-    <View>
-      {/* <Text>You are Signed in</Text>
-      <Link href="/profile" asChild>
-        <Pressable>
-          <Text>Profile</Text>
-        </Pressable>
-      </Link> */}
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState<string>("null");
 
-      <MapView />
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        // initialRegion={{
+        //   latitude: 37.78825,
+        //   longitude: -122.4324,
+        //   latitudeDelta: 0.0922,
+        //   longitudeDelta: 0.0421,
+        // }}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={true}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    position: "relative",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+
+    // position: "absolute",
+  },
+});
