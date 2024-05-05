d0a79a9c6c371ded5f66d7b0d75ac028e7f479e7
Faisal Sayed <faisal.sayed502@gmail.com>
2024-05-05 16:24:51 -0400

    feat: temporary settings page

--- expo/app/group/[id]/settings.tsx
+++ expo/app/group/[id]/settings.tsx
@@ -1,25 +1,40 @@
 import { useAuth } from "@clerk/clerk-expo";
-import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams } from "expo-router";
 import * as React from "react";
-import { Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 const SettingsPage = () => {
   const { getToken } = useAuth();
-  const { id } = useLocalSearchParams();
+  const params = useLocalSearchParams();
+  const group = JSON.parse((params.group as string) || "{}");
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["group", id],
-    queryFn: async () => {
-      const token = await getToken();
-      const response = await fetch(`http://localhost:5000/group/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      return response.json();
-    },
-  });
+  const copyToClipboard = async () => {
+    if (!group.code) return;
+    await Clipboard.setStringAsync(group.code.toString());
+    alert("Group code copied to clipboard!");
+  };
 
-  return <Text className="text-xl text-red-400">SettingsPage</Text>;
+  return (
+    <View className="p-10 flex flex-col justify-between h-full">
+      <View className="flex flex-col gap-2">
+        <Pressable
+          className="p-5 bg-gray-200 rounded-md border"
+          onPress={copyToClipboard}
+        >
+          <Text className="text-center">Invite Your Friends</Text>
+        </Pressable>
+        <View className="p-5 bg-orange-500/30 border border-orange-500 rounded-md">
+          <Text className="text-center tracking-[12px] text-2xl font-black text-orange-600">
+            {group.code}
+          </Text>
+        </View>
+      </View>
+      <Pressable className="p-5 bg-gray-200 rounded-md border">
+        <Text className="text-center">Leave Group</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default SettingsPage;
